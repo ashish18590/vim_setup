788ad837fc32cb8cf3217ec1d25abedd25c97887
ashish3 <ashish@intel.com>
2018-06-05 19:10:31 +0530

    Added "clean" module for new setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+if [ "$1" == "clean" ]; then
+	if [ -e $HOME/.vim/bundle ]; then
+		cp -rv $HOME/.vim/bundle $HOME/vim/bundle_bkp
+		rm -rf $HOME/.vim/bundle
+	fi
+	if [ -e $HOME/.vimrc ]; then
+		cp -rv $HOME/.vimrc $HOME/vimrc_bkp
+		rm -rf $HOME/.vimrc
+	fi
+
+fi
+
 mkdir -p $HOME/.vim/bundle
 if [ $? -ne 0 ]; then
 	echo "Unable to create directory : $HOME/.vim/bundle... Error"
-	exit 1
-fi
-mkdir -p $HOME/system_setup
-if [ $? -ne 0 ]; then
-	echo "Unable to create directory : mkdir -p $HOME/system_setup... Error"
 	exit 1
 fi
 
